1c57cc00c03636ccc481c78d97d9ab8f4fc55f89
LeiamNash <94122613+LeiamNashRebirth@users.noreply.github.com>
2023-09-03 17:53:50 +0800

    Create   te.js

---
+++ te.js
@@ -0,0 +1,30 @@
+ const fs = require("fs");
+ const axios = require("axios");
+
+function leiamnash(){
+ return{
+  "name": "cosplay",
+  "author": "leiamnash",
+  "version": "1.0.0",
+  "commandMap": {
+  "cosplay": {
+    "func": "cosplay",
+    "cooldown": 0
+   }
+  }
+ }
+}
+
+async function cosplay(event, api, leiam, log) {
+try{
+ 
+ } catch (err) { 
+  log.err(err); 
+  api.chat(`Error: {\nstatus: 9299\nsummary: {\n'leiamnash server is offline',\nconnection refuse to response,\n},\nhttp: cannot get data from leiamnash server\n}`, event.threadID, () => api.setMessageReaction("❎", event.messageID, (err) => {}, true), event.messageID);
+ } 
+}
+
+module.exports = {
+    cosplay,
+    leiamnash
+}
